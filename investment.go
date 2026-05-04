261fa2e1d8af62371f732ae9f4a8946d74da8941
adara koko <adarakoko30@gmail.com>
2026-05-04 14:18:08 -0500

    investment calculator

--- investment.go
+++ investment.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	 investmentAmount  := 1000.0
 	 expectedReturnRate := 5.5
 	 Years  := 10.0
 
 	 futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, Years)
-	 futureRealValue := futureValue / 
-	fmt.Println(futureValue)
+	 futureRealValue := futureValue /  math.Pow(1+inflationRate/100, Years)
+
+	 fmt.Println(futureValue)
+	 fmt.Println(futureRealValue)
 }
