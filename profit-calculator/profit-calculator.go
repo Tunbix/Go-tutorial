a0869eadf604bed69083c6c567f11c249158e77d
adara koko <olusimbo4aina@gmail.com>
2026-05-05 16:33:01 -0500

    upated the profit calculator

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -1,34 +1,30 @@
 package main
 
 import (
-
 	"fmt"
-
-	
 )
 
-func main()  {
+func main() {
 
 	var revenue float64
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Revenue Income")
+	fmt.Print("Revenue Income: ")
 	fmt.Scan(&revenue)
 
-	fmt.Print("Expenses")
+	fmt.Print("Expenses: ")
 	fmt.Scan(&expenses)
 
-	fmt.Print("Tax Rate")
+	fmt.Print("Tax Rate: ")
 	fmt.Scan(&taxRate)
-	
-	 revenueBeforeTax := revenue - expenses
-	 revenueAfterTax := revenueBeforeTax - taxRate
-	 incomeRatio := revenueBeforeTax / revenueAfterTax
 
-	 fmt.Println(revenueBeforeTax)
-	 fmt.Println(revenueAfterTax)
-	 fmt.Println(incomeRatio)
+	revenueBeforeTax := revenue - expenses
+	revenueAfterTax := revenueBeforeTax * (1 - taxRate/100)
+	incomeRatio := revenueBeforeTax / revenueAfterTax
 
-	
+	fmt.Println(revenueBeforeTax)
+	fmt.Println(revenueAfterTax)
+	fmt.Println(incomeRatio)
+
 }
