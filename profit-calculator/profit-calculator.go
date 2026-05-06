ecd61f69e94a94f72001e0abbfd6ffb69c5ddbb3
adara koko <olusimbo4aina@gmail.com>
2026-05-06 18:01:38 -0500

    upgrade the profitCalculator using muliti

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -10,21 +10,36 @@ func main() {
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Revenue Income: ")
+	//fmt.Print("Revenue Income: ")
+	outputText("Revenue Income: ")
 	fmt.Scan(&revenue)
 
-	fmt.Print("Expenses: ")
+	//fmt.Print("Expenses: ")
+	outputText("Expenses: ")
 	fmt.Scan(&expenses)
 
-	fmt.Print("Tax Rate: ")
+	//fmt.Print("Tax Rate: ")
+	outputText("Tax Rate: ")
 	fmt.Scan(&taxRate)
 
 	revenueBeforeTax := revenue - expenses
 	revenueAfterTax := revenueBeforeTax * (1 - taxRate/100)
 	incomeRatio := revenueBeforeTax / revenueAfterTax
+    //outputData information
+	//fmt.Println("Tax Ratio:)
+	//fmt.Println("Profit:", revenueAfterTax)
+	//fmt.Println("Tax Ratio:", incomeRatio)
 
-	fmt.Println(revenueBeforeTax)
-	fmt.Println(revenueAfterTax)
-	fmt.Println(incomeRatio)
+	//fmt.Printf("RBT: %.0f\nProfit: %.0f\nTax Ratio: %.0f", revenueBeforeTax, revenueAfterTax, incomeRatio) 
 
+	formattedBT := fmt.Sprintf("RBT: %.1f\n", revenueBeforeTax)
+	formattedPT := fmt.Sprintf("Profit: %.1f\n", revenueAfterTax)
+	formattedTR := fmt.Sprintf("Tax Ratio: %.1f\n", incomeRatio)
+
+	fmt.Print(formattedBT, formattedPT, formattedTR)
+
+	func outputText(text string) {
+		fmt.Print(text)
+
+	}
 }
