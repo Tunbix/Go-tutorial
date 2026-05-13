f3011dd6e7ac04075a9105402e4780b19644ef25
adara koko <olusimbo4aina@gmail.com>
2026-05-13 12:13:33 -0500

    update the calculator using the if statement and error handing

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -2,59 +2,82 @@ package main
 
 import (
 	"fmt"
-
-	
+	"os"
+	"errors"
 )
 
+func storeResult(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio )
+	os.WriteFile("result.txt", []byte(results), 0644)
+}
+
 func main() {
+	revenue, err := getUserInput("Revenue: ")
 
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	//fmt.Print("Revenue Income: ")
-	outputText("Revenue Income: ")
-	fmt.Scan(&revenue)
+	expenses, err := getUserInput("Expenses: ")
 
-	//fmt.Print("Expenses: ")
-	outputText("Expenses: ")
-	fmt.Scan(&expenses)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	//fmt.Print("Tax Rate: ")
-	outputText("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	taxRate , err:= getUserInput("Tax Rate: ")
+	
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	
 
 	calculateEarings(revenue, expenses, taxRate)
-	revenueBeforeTax := revenue - expenses
-	revenueAfterTax := revenueBeforeTax * (1 - taxRate/100)
-	incomeRatio := revenueBeforeTax / revenueAfterTax
-    //outputData information
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	
+	ratio := ebt / profit
+	
+	//outputData information
 	//fmt.Println("Tax Ratio:)
 	//fmt.Println("Profit:", revenueAfterTax)
 	//fmt.Println("Tax Ratio:", incomeRatio)
 
-	//fmt.Printf("RBT: %.0f\nProfit: %.0f\nTax Ratio: %.0f", revenueBeforeTax, revenueAfterTax, incomeRatio) 
+	//fmt.Printf("RBT: %.0f\nProfit: %.0f\nTax Ratio: %.0f", revenueBeforeTax, revenueAfterTax, incomeRatio)
 
-	formattedBT := fmt.Sprintf("RBT: %.1f\n", revenueBeforeTax)
-	formattedPT := fmt.Sprintf("Profit: %.1f\n", revenueAfterTax)
-	formattedTR := fmt.Sprintf("Tax Ratio: %.1f\n", incomeRatio)
+	formattedBT := fmt.Sprintf("RBT: %.1f\n", ebt)
+	formattedPT := fmt.Sprintf("Profit: %.1f\n", profit)
+	formattedTR := fmt.Sprintf("Tax Ratio: %.1f\n", ratio)
+
+	storeResult(ebt, profit, ratio)
 
 	fmt.Print(formattedBT, formattedPT, formattedTR)
 
-	
-
-	
 }
 
-func outputText(text string)  {
-		fmt.Print(text)
+func outputText(text string) {
+	fmt.Print(text)
 }
 
 func calculateEarings(revenue, expenses, taxRate float64) (float64, float64, float64) {
-		bt := revenue - expenses
-		pt := bt * (1 - taxRate/100)
-		tr := bt / pt
+	bt := revenue - expenses
+	pt := bt * (1 - taxRate/100)
+	tr := bt / pt
 
-		return bt, pt, tr 
+	return bt, pt, tr
+}
+
+func getUserInput(infoText string) (float64, error)  {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+
+	if userInput <= 0{
+		return 0, errors.New("Value must be a postive number")
 	}
 
+	return userInput, nil
+}
