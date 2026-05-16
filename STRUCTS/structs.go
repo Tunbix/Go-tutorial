00b887bc55a1ff49fede42a3e6f3d89a11ca5cd1
adara koko <olusimbo4aina@gmail.com>
2026-05-15 21:08:57 -0500

    working with pionters

--- STRUCTS/structs.go
+++ STRUCTS/structs.go
@@ -2,13 +2,30 @@ package main
 
 import(
 	"fmt"
+	"time"
 )
+
+type user struct{
+	firstName string
+	lastName string
+	birthdate string
+	createdAt time.Time
+}
 
 func main() {
 
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdaten (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdaten (MM/DD/YYYY): ")
+
+	var appUser user
+
+	appUser = user{
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
 
 	fmt.Println(firstName, lastName, birthdate)
 
