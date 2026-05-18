bb605d05ebc38324b186b1438abee38d3e677720
adara koko <olusimbo4aina@gmail.com>
2026-05-18 11:49:26 -0500

    using constuctor function

--- STRUCTS/structs.go
+++ STRUCTS/structs.go
@@ -12,6 +12,25 @@ type user struct{
 	createdAt time.Time
 }
 
+func (u user) outputUserDetails()  {
+	
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *user) clearUserName()  {
+	u.firstName = ""
+	u.lastName =""
+}
+
+func newUser(firstName, lastName, birthdate string) user  {
+	return  user{
+		firstName: firstName,
+		lastName: lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}
+}
+
 func main() {
 
 	userFirstName := getUserData("Please enter your first name: ")
@@ -20,16 +39,19 @@ func main() {
 
 	var appUser user
 
-	appUser = user{
-		firstName: userFirstName,
-		lastName: userLastName,
-		birthdate: userBirthdate,
-		createdAt: time.Now(),
-	}
 
-	fmt.Println(firstName, lastName, birthdate)
+	appUser = newUser(userFirstName, userLastName, userBirthdate)
+	
+
+	//... do something awesome with that gathered data!
+
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
 
 }
+
+
 
 func getUserData(promptText string) string {
 
