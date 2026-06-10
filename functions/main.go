1aa2a4fa5d2c4b16d81acd6c22e3f990e0b0bab3
adara koko <olusimbo4aina@gmail.com>
2026-06-10 01:58:36 -0500

    break down of function in function...in go

--- functions/main.go
+++ functions/main.go
@@ -4,34 +4,18 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	sum := sumup(22, 10, 9, 3, 8)
+	anotherSum := sumup(22, numbers...)
 
-	double := createTransformFunc(2)
-	triple := createTransformFunc(3)
-
-	transformed := transformNumber(&numbers, func(num int) int {
-		return num * 2
-	})
-
-	doubled := transformNumber(&numbers, double)
-	tripled := transformNumber(&numbers, triple)
-
-	fmt.Println(transformed)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
-func transformNumber(numbers *[]int, transformFunc func(int) int) []int {
-	dNumbers := []int{}
+func sumup(startingValue int, numbers ...int) int {
+	sum := 0
 
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transformFunc(val))
+	for _, val := range numbers {
+		sum += val // sum = sum + val
 	}
-
-	return dNumbers
-}
-
-func createTransformFunc(factor int) func(int) int {
-	return func(num int) int {
-		return num * factor
-	}
+	return sum
 }
